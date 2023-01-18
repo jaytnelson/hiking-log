3149196b51aeb80af255e54e2ad8a9641a6b0ccc
J <j@Js-MacBook-Pro.local>
2023-01-18 10:50:59 -0800

    add eslint

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="hikes" element={<HikeList />} />
+          <Route index element={<Home />} />
+          <Route path="hikes" element={<HikeList />} />
         </Route>
       </Routes>
     </Router>
